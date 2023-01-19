303c601f2381c8c839c385204c7b749953143443
Luan Cardoso <luancardosoti@gmail.com>
2023-01-18 22:04:22 -0300

    Aula 01: iniciando o projeto de ponta a ponta - web e server

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import { Habit } from "./components/Habit";
+
+export function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+    </div>
+  )
+}
